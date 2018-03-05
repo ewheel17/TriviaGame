d77878b6111d429b3242a15bfee4e8c313b7d473
Max <u1110071@utah.edu>
2018-03-05 15:45:42 -0700

    3.5.2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,64 @@
 var totalWin;
 var totalLoss;
+var totalUnans;
 var gameNum;
+var a;
 
+// Starts the game by setting the values and beginning the question "cycle".
 function startGame() {
     totalWin = 0;
     totalLoss = 0;
+    totalUnans = 0;
     gameNum = 1;
 
+    $(".startBtn").hide();
     cycleGame();
-
 }
-
-function wrongMove() {
-    totalLoss++;
-    
-}
-
-function correctMove() {
-    totalWin++;
-
-}
+// -------------------------------------------------------------------------
 
 function cycleGame() {
-    
+    a = ".question" + String(gameNum);
+    $(a).show();
+
+    console.log("Total Losses: " + totalLoss)
+    console.log("Total Wins: " + totalWin)
 }
 
+// If an answer choice is good/bad, adjust scores accordingly.
+$(".badMove").click(function(){
+    totalLoss++;
+    $(a).hide();
+    gameNum++;
 
+    if (gameNum > 10) {
+        $("#finalScreen").show();
+        $("#totCorrect").html("<h2>Correct: </h2>" + totalWin);
+        $("#totWrong").html("<h2>Incorrect: </h2>" + totalLoss);
+        $("#totUnans").html("<h2>Unanswered: </h2>" + totalUnans);
+        return;
+    }
+
+    cycleGame();
+})
+
+$(".goodMove").click(function(){
+    totalWin++;
+    $(a).hide();
+    gameNum++;
+
+    if (gameNum > 10) {
+        $("#finalScreen").show();
+        $("#totCorrect").html("<h2>Correct: </h2>" + totalWin);
+        $("#totWrong").html("<h2>Incorrect: </h2>" + totalLoss);
+        $("#totUnans").html("<h2>Correct: </h2>" + totalUnans);
+        return;
+    }
+
+    cycleGame();
+})
+// ----------------------------------------------------------
+
+// Hiding all boxes initially. Display them as needed.
 $(".question1").hide();
 $(".question2").hide();
 $(".question3").hide();
@@ -36,7 +69,11 @@ $(".question7").hide();
 $(".question8").hide();
 $(".question9").hide();
 $(".question10").hide();
+$("#afterBox1").hide();
+$("#finalScreen").hide();
+//----------------------------------------------------
 
+// Mouseover Highlight for potential answers.
 $(".ans1").mouseover(function () {
     $(".ans1").addClass("highlight");
 });
@@ -64,3 +101,4 @@ $(".ans4").mouseover(function () {
 $(".ans4").mouseout(function () {
     $(".ans4").removeClass("highlight");
 });
+// ----------------------------------------------------
