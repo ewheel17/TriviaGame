59655ac635000e07a778e13f25b19f0924df04c3
Max <u1110071@utah.edu>
2018-03-05 19:48:57 -0700

    3.5.3

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,8 @@ var totalLoss;
 var totalUnans;
 var gameNum;
 var a;
+var timer;
+var intervalId;
 
 // Starts the game by setting the values and beginning the question "cycle".
 function startGame() {
@@ -10,19 +12,60 @@ function startGame() {
     totalLoss = 0;
     totalUnans = 0;
     gameNum = 1;
+    timer = 30;
 
+    $("#finalScreen").hide();
     $(".startBtn").hide();
     cycleGame();
 }
 // -------------------------------------------------------------------------
 
 function cycleGame() {
+    $("#afterBox1").hide();
+    $("#afterBox2").hide();
+    $("#afterBox3").hide();
+    
     a = ".question" + String(gameNum);
     $(a).show();
 
-    console.log("Total Losses: " + totalLoss)
-    console.log("Total Wins: " + totalWin)
+    intervalId = setInterval(count, 1000);
 }
+
+// Functions affiliated with the Timer.
+
+function count() {
+    timer --;
+    var theTime = timer;
+    var t = timeConvert(theTime);
+    
+    $(".theTimer").html(t);
+    
+    if (timer <= 0) {
+        timeOut();
+    }
+}
+
+function timeConvert(t) {
+
+    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+// ----------------------------------------------
 
 // If an answer choice is good/bad, adjust scores accordingly.
 $(".badMove").click(function(){
@@ -30,15 +73,29 @@ $(".badMove").click(function(){
     $(a).hide();
     gameNum++;
 
-    if (gameNum > 10) {
-        $("#finalScreen").show();
-        $("#totCorrect").html("<h2>Correct: </h2>" + totalWin);
-        $("#totWrong").html("<h2>Incorrect: </h2>" + totalLoss);
-        $("#totUnans").html("<h2>Unanswered: </h2>" + totalUnans);
-        return;
-    }
+    $("#afterBox1").show();
+    $("#afterBox2").show();
 
-    cycleGame();
+    clearInterval(intervalId);
+    timer = 30;
+    var t = timeConvert(30);
+    $(".theTimer").html(t);
+   
+    setTimeout(function() {
+        if (gameNum > 10) {
+
+            $("#afterBox1").hide();
+            $("#afterBox3").hide();
+            $("#finalScreen").show();
+            $("#totCorrect").html("<h2>Correct: </h2>" + totalWin);
+            $("#totWrong").html("<h2>Incorrect: </h2>" + totalLoss);
+            $("#totUnans").html("<h2>Unanswered: </h2>" + totalUnans);
+            
+            return;
+        } else {
+            cycleGame();
+        }
+    }, 1000);
 })
 
 $(".goodMove").click(function(){
@@ -46,16 +103,60 @@ $(".goodMove").click(function(){
     $(a).hide();
     gameNum++;
 
-    if (gameNum > 10) {
-        $("#finalScreen").show();
-        $("#totCorrect").html("<h2>Correct: </h2>" + totalWin);
-        $("#totWrong").html("<h2>Incorrect: </h2>" + totalLoss);
-        $("#totUnans").html("<h2>Correct: </h2>" + totalUnans);
-        return;
-    }
+    $("#afterBox1").show();
+    $("#afterBox3").show();
 
-    cycleGame();
+    clearInterval(intervalId);
+    timer = 30;
+    var t = timeConvert(30);
+    $(".theTimer").html(t);
+
+    setTimeout(function() {
+        if (gameNum > 10) {
+
+            $("#afterBox1").hide();
+            $("#afterBox3").hide();
+            $("#finalScreen").show();
+            $("#totCorrect").html("<h2>Correct: </h2>" + totalWin);
+            $("#totWrong").html("<h2>Incorrect: </h2>" + totalLoss);
+            $("#totUnans").html("<h2>Unanswered: </h2>" + totalUnans);
+            
+            return;
+        } else {
+            cycleGame();
+        }
+    }, 1000);
 })
+
+function timeOut() {
+    totalUnans++;
+    $(a).hide();
+    gameNum++;
+
+    $("#afterBox1").show();
+    $("#afterBox2").show();
+
+    clearInterval(intervalId);
+    timer = 30;
+    var t = timeConvert(30);
+    $(".theTimer").html(t);
+   
+    setTimeout(function() {
+        if (gameNum > 10) {
+
+            $("#afterBox1").hide();
+            $("#afterBox3").hide();
+            $("#finalScreen").show();
+            $("#totCorrect").html("<h2>Correct: </h2>" + totalWin);
+            $("#totWrong").html("<h2>Incorrect: </h2>" + totalLoss);
+            $("#totUnans").html("<h2>Unanswered: </h2>" + totalUnans);
+            
+            return;
+        } else {
+            cycleGame();
+        }
+    }, 1000);
+}
 // ----------------------------------------------------------
 
 // Hiding all boxes initially. Display them as needed.
